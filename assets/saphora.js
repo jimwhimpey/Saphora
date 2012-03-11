9f37fc818c15045faf125883f66ab0528021d52a
Jim Whimpey <jimwhimpey@me.com>
2012-03-11 20:28:13 +1000

    Got the update and checkout buttons working on the cart page.

--- assets/saphora.js
+++ assets/saphora.js
@@ -5,5 +5,19 @@ $(function(){
 		$("form.add-to-cart").submit();
 		return false;
 	});
+	
+	// Handler for updating the cart
+	$(".cart tr.total td.quantity a").click(function(){
+		$("form").append('<input type="hidden" name="update" value="Update" />');
+		$("form").submit();
+		return false;
+	});
+
+	// Handler for checking out the cart
+	$("a.checkout-button").click(function(){
+		$("form").append('<input type="hidden" name="checkout" value="Checkout" />');
+		$("form").submit();
+		return false;
+	});
 
 });
